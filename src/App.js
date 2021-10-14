6146ccf226c9e602f77850f5af7d88ef3268bdd2
maribelcuales <maribelcuales@gmail.com>
2021-10-14 14:51:54 +0800

    Add useEffect to fetch data from the Final Space character endpoint.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://finalspaceapi.com/api/v0/character/?limit=12")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+  
   return (
     <div> 
-      Hello hello! 
+      <h1>Hello hello!</h1> 
     </div>
   );
 }
