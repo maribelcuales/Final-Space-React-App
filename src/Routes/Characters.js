00dac7703b3faed8d8752482182f08a938a51961
maribelcuales <maribelcuales@gmail.com>
2021-10-16 23:55:13 +0800

    Created Characters component fetching the Final Space characters.

---
+++ src/Routes/Characters.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import Container from "@material-ui/core/Container";
+import { Typography } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import { CardMedia } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+    boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+    backgroundColor: "#fafafa",
+  },
+  media: {
+    height: "300px",
+  },
+});
+
+function Characters() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://finalspaceapi.com/api/v0/character/?limit=12")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Container>
+        <Typography
+          color="textPrimary"
+          gutterBottom
+          variant="h2"
+          align="center"
+        >
+          Final Space React App with Material UI
+        </Typography>
+        <Grid container spacing={3}>
+          {data.map((character) => (
+            <Grid item xs={12} sm={4} key={character.id}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.media}
+                  image={character.img_url}
+                />
+                <CardContent>
+                  <Typography color="primary" variant="h5">
+                    {character.name}
+                  </Typography>
+                  <Typography color="textSecondary" variant="subtitle2">
+                    {character.status}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default Characters;
