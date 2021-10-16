05967fa62e0f9a3a6c9b7a335b9a40c47360d42a
maribelcuales <maribelcuales@gmail.com>
2021-10-16 23:35:25 +0800

    Created Location component.

---
+++ src/Routes/Location.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import Container from "@material-ui/core/Container";
+import { Typography } from "@material-ui/core";
+
+const Location = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://finalspaceapi.com/api/v0/location/?limit=9")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <div>
+      <Container>
+        <Typography>Final Space Location</Typography>
+      </Container>
+    </div>
+  );
+};
+
+export default Location;
