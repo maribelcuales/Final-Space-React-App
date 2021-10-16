900cdc246c6df1202104b88d3e442633d5e9343e
maribelcuales <maribelcuales@gmail.com>
2021-10-16 23:15:16 +0800

    Added Router, Switch, and Route then set route path for App component.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, Switch } from 'react-router-dom';
 
 import App from './App';
+import Episodes from './Routes/Episodes.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch> 
+  </Router>,
   document.getElementById('root')
 );
