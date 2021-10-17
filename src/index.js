a7d95c32cb9f8701913624c25863e81e9ee7d0f5
maribelcuales <maribelcuales@gmail.com>
2021-10-17 15:43:06 +0800

    Import BrowserRouter as Router and wrapped the App component with it.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, Switch } from "react-router-dom";
-
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-import Episodes from "./Routes/Episodes.js";
 
 ReactDOM.render(
   <Router>
-    <Switch>
-      <Route path="/episodes" component={Episodes} />
-      <Route path="/c0haracter" component={Character} />
-      <Route path="/location" component={Location} />
-      <Route path="/" component={App} />
-    </Switch>
+    <App />
   </Router>,
   document.getElementById("root")
 );
